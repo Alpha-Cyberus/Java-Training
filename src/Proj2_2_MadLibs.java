a0652f16f6e98b6cf5b11862ae5ed7d39165a566
Alpha-Cyberus <ammonobrien@gmail.com>
2021-11-24 00:57:22 +0100

    Add progress update through course

---
+++ src/Proj2_2_MadLibs.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Proj2_2_MadLibs {
+    public static void main (String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        String adj1, adj2, adj3, bName, cloth, gName, hobby, mName, occ1, occ2, place;
+
+        System.out.println("Enter an adjective");
+        adj1  = keyboard.nextLine();
+        System.out.println("Enter an adjective");
+        adj2  = keyboard.nextLine();
+        System.out.println("Enter an adjective");
+        adj3  = keyboard.nextLine();
+        System.out.println("Enter a boy's name");
+        bName  = keyboard.nextLine();
+        System.out.println("Enter a piece of clothing");
+        cloth  = keyboard.nextLine();
+        System.out.println("Enter a girl's name");
+        gName  = keyboard.nextLine();
+        System.out.println("Enter a hobby");
+        hobby  = keyboard.nextLine();
+        System.out.println("Enter a man's name");
+        mName  = keyboard.nextLine();
+        System.out.println("Enter an occupation");
+        occ1  = keyboard.nextLine();
+        System.out.println("Enter an occupation");
+        occ2  = keyboard.nextLine();
+        System.out.println("Enter a place");
+        place  = keyboard.nextLine();
+
+        System.out.println("There once was a " + adj1 + " girl named " + gName + ", who was a " + adj2 + " " + occ1 + " in the Kingdom of " + place + ". She loved to wear " + cloth + " and to " + hobby + ".");
+        System.out.println("She wanted to marry the " + adj3 + " " + occ2 + " named " + bName + " but her father, King " + mName + " forbid her from seeing him.");
+    }
+}
---
+++ src/Proj3_1_LearningPackages.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Proj3_1_LearningPackages {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Which package? (1-3)");
+        int pkg = keyboard.nextInt();
+        while(pkg < 1 || pkg > 3) {
+            System.out.println("Which package? (1-3)");
+            pkg = keyboard.nextInt();
+        }
+
+        System.out.println("How many courses?");
+        int crs = keyboard.nextInt();
+        int crsExtra = 0;
+
+        switch(pkg){
+            case 1: // $10/month, inc. 2 courses, $6 per additional course
+                if (crs > 2) {
+                    crsExtra = crs - 2;
+                }
+                System.out.println("Total cost: " + (10 + (crsExtra*6)));
+                break;
+            case 2: // $12/month, inc. 4 courses, $4 per additional course
+                if (crs > 4) {
+                    crsExtra = crs - 4;
+                }
+                System.out.println("Total cost: " + (12 + (crsExtra*4)));
+                break;
+            case 3: // $15/month, inc. 6 courses, $3 per additional course
+                if (crs > 6) {
+                    crsExtra = crs - 6;
+                }
+                System.out.println("Total cost: " + (15 + (crsExtra*3)));
+                break;
+        }
+    }
+}
