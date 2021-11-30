9500f6ad7bbc3b1f20b78eded320cbe8ff6f7ecb
Alpha-Cyberus <ammonobrien@gmail.com>
2021-11-30 14:09:23 +0100

    Add progress update through course Complete tic tac toe functionality. bug01: does not handle non-numeric input.

--- src/TheCompleteJavaDeveloperCourseOReilly/Proj6_2_TicTacToe.java
+++ src/TheCompleteJavaDeveloperCourseOReilly/Proj6_2_TicTacToe.java
@@ -1,17 +1,35 @@
 package TheCompleteJavaDeveloperCourseOReilly;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Proj6_2_TicTacToe {
     public static void main(String[] args) {
-        char currPlayer = 'X';
-        char[][] gameGrid = initGrid(); // Prepare blank grid
-        printGrid(gameGrid); // Display grid
+        boolean gameOver;
+        char currentPlayer = 'X'; // as cP
+        char[][] gameGrid; // as gG
+        int[] playerCoordinate; // as pC
 
-        int[] pC = playerCoordinate(currPlayer); // Request and return player input for selected coordinate
-        gameGrid[pC[0]][pC[1]] = currPlayer;
+        { // Initialize game
+            gameGrid = initGrid(); // Prepare blank grid
+            printGrid(gameGrid); // Display grid
+        }
 
+        do{ // Game cycle, run until winner is detected
+            currentPlayer = setPlayer(currentPlayer); // Set current player
+
+            System.out.println("It's " + currentPlayer + "'s turn");
+            System.out.println("Enter a number (1-3) for across THEN down, separated by a space.");
+
+            playerCoordinate = playerInput(gameGrid); // Process player input as coordinate
+            gameGrid[playerCoordinate[0]][playerCoordinate[1]] = currentPlayer; // Input pC into gG
+            printGrid(gameGrid); // Display grid
+
+            gameOver = checkState(gameGrid);// Check if someone wins
+        } while (!gameOver);
+
+        System.out.println("Game over. " + currentPlayer + " is the winner. Congratulations!");
     }
 
     public static char[][] initGrid() {
@@ -38,33 +56,73 @@ public class Proj6_2_TicTacToe {
         }
     } //end printGrid
 
-    public static int[] playerCoordinate(char pTurn) {
+    public static int[] playerInput(char[][] gG) {
         Scanner kbd = new Scanner(System.in);
+        String input = null;
+        boolean goodInput = false;
         int[] pI = new int[2];
-
-        System.out.println("It's " + pTurn + "'s turn");
-        System.out.println("Enter a number (1-3) for the row THEN the column, separated by a space.");
-
+        
         do {
-            String input = kbd.nextLine();
-
+            // Catch non-integer input
+//            while (!goodInput) {
+//                try {
+//                    input = String.valueOf(kbd.nextInt());
+//                    goodInput = true;
+//                } catch (InputMismatchException e) {
+//                    System.out.println("Only enter numbers and a space.");
+//                    kbd.nextLine();
+//                }
+//            }
+            input = kbd.nextLine();
+            System.out.println(input);
             if (input.length() > 3) {
-                System.out.println("Input too long! Try again.");
+                System.out.println("Input must be: number space number! Try again.");
                 continue;
             }
-            pI[0] = Integer.parseInt(input.substring(0,1));
-            pI[1] = Integer.parseInt(input.substring(2));
-            if (pI[0] < 1 || pI[0] > 3) {
-                System.out.println(
-                        "First number is not between 1 and 3, try again.");
-            continue;
-            } else if (pI[1] < 1 || pI[1] > 3) {
+            // gG ranges from 0-2 but 1-3 is easier for brain to understand,
+            // so take 1-3 as input and -1 to make it fit the array.
+            pI[1] = Integer.parseInt(input.substring(0,1)) - 1;
+            pI[0] = Integer.parseInt(input.substring(2)) - 1;
+            if (pI[1] < 0 || pI[1] > 2) {
+                System.out.println("First number is not between 1 and 3, try again.");
+                continue;
+            } else if (pI[0] < 0 || pI[0] > 2) {
                 System.out.println("Second number is not between 1 and 3, try again.");
-            continue;
+                continue;
+            } else if (gG[pI[0]][pI[1]] != ' ') {
+                System.out.println("This space is taken, try again");
+                continue;
             }
             break;
         } while (true);
 
         return pI;
     } // end playerCoordinate
+
+    public static boolean checkState(char[][] gG) {
+        // if first space not empty and whole row or column matches
+        for (int i = 0; i < 3; i++) {
+            if (gG[i][0] != ' ' && gG[i][0] == gG[i][1] && gG[i][1] == gG[i][2]) {
+                return true;
+            } else if (gG[0][i] != ' ' && gG[0][i] == gG[1][i] && gG[1][i] == gG[2][i]) {
+                return true;
+            }
+        }
+        // if corner not empty and whole diagonal matches
+        if (gG[0][0] != ' ' && gG[0][0] == gG[1][1] && gG[1][1] == gG[2][2]) {
+            return true;
+        } else if (gG[0][2] != ' ' && gG[0][2] == gG[1][1] && gG[1][1] == gG[2][0]) {
+            return true;
+        }
+        return false;
+    } // end checkState
+
+    public static char setPlayer(char cP) {
+            if (cP == 'X') {
+                cP = 'O';
+            } else {
+                cP = 'X';
+            }
+        return cP;
+    } // end setPlayer
 }
