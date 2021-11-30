96d78a6faec32e34cd5aa490fd2d47254f5e53ec
Alpha-Cyberus <ammonobrien@gmail.com>
2021-11-30 19:22:18 +0100

    Add progress update through course. Currently: Finished Chapter 7, Project 1

---
+++ src/TheCompleteJavaDeveloperCourseOReilly/Proj7_1_BankAccountDemo.java
@@ -0,0 +1,15 @@
+package TheCompleteJavaDeveloperCourseOReilly;
+
+import java.util.Arrays;
+
+public class Proj7_1_BankAccountDemo {
+    public static void main(String[] args) {
+        BankAccount myBank = new BankAccount("Luffy", 15);
+
+        System.out.println(myBank.getBalance());
+
+        myBank.deposit(50);
+
+        myBank.withdraw(250);
+    }
+}
